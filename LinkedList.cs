ad91cdecf3846f3e18549b626b2c8ade647382c7
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-25 21:02:25 +0530

    added: New Linked List logic

---
+++ LinkedList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14LinkedList
+{
+    public class LinkedList
+    {
+        public Node Head;
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public void PrintList()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
