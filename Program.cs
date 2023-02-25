680d98a2bfd7cb94b651f69deac03415cb861020
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-25 21:01:32 +0530

    added: New LinkedList Created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Day14LinkedList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            list.PrintList();
+        }
+    }
+}
